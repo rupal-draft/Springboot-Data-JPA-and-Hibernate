bac35343d3bd5c316805e2a6746d3ba4983337e8
Rupal Paul <rupalpaulgspr@gmail.com>
2024-10-22 00:23:26 -0700

    Initial Setup

---
+++ src/main/java/com/springbootJPA/hibernate/Springboot/Data/JPA/and/Hibernate/SpringbootDataJpaAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.springbootJPA.hibernate.Springboot.Data.JPA.and.Hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDataJpaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDataJpaAndHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootJPA/hibernate/Springboot/Data/JPA/and/Hibernate/SpringbootDataJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootJPA.hibernate.Springboot.Data.JPA.and.Hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDataJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
